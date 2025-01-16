4931044841aa296b8f6f73e632c701119f33f8ac
CharanTeja <charanteja11432@gmail.com>
2025-01-16 15:24:14 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Head from './port/head';
+
 import Home from './port/home';
 function App() {
   return (
     <div className="App">
       <Home/>
-      <Head/>
+      
     </div>
   );
 }
